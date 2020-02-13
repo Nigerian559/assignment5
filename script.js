b4fa69a15558db3820e33fd52f01d17d32e5b2a3
Olukoya <alexolukoya@my.whccd.edu>
2020-02-12 20:52:44 -0800

    the second question i did it wrong, make sure to fix at home

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ for(let i = start; i <= end; i++)
   return myArray
 }
 console.log(range(1,10));
+////////////////////////////////////////////////////// 
+const sum = function(arrayNum)
+{
+    let aSum = 0;
+    for(let j = 0; j <= arrayNum.length-1; j++)
+    {
+        aSum += arrayNum[j];
+    };
+    return aSum
+};
+console.log(sum(range(1, 10)));
 
